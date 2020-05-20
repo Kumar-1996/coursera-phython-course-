3c517104749cb1f4efac21f7d5f99614e8996e81
Rishabh Kumar <65464723+Kumar-1996@users.noreply.github.com>
2020-05-21 00:11:05 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Sum {
+public static void main(String[] args) {
+	
+Scanner  sc= new Scanner(System.in);
+int a,b,sum;
+System.out.println("enter the a:");
+a=sc.nextInt();
+System.out.println("enter the b:");
+b=sc.nextInt();
+sum = a+b;
+
+System.out.println("addition value is-"+ sum);
+
+}
+
+
+
+}
