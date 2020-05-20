476e1b6f1ecc6dcfc61593fdb4383fe5da692840
Rishabh Kumar <65464723+Kumar-1996@users.noreply.github.com>
2020-05-21 00:36:30 +0530

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Subtract{
+public static void main(String[] args) {
+	
+Scanner sc= new Scanner(System.in);
+int a,b,sub;
+System.out.println("enter the vvalue:");
+a= sc.nextInt ();
+System.out.println("enter the value:");
+b= sc.nextInt ();
+
+sub=a-b;
+
+System.out.println(+sub);
+
+}
+
+
+
+}
+
