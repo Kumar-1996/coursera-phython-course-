bdb806819c32cdae607c90b217f690b0ffe319b1
Rishabh Kumar <65464723+Kumar-1996@users.noreply.github.com>
2020-05-21 00:56:27 +0530

    Add files via upload

---
+++ Divide.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Divide {
+public static void main(String[] args) {
+	
+Scanner  sc= new Scanner(System.in);
+int a,b,div;
+System.out.println("enter the a:");
+a=sc.nextInt();
+System.out.println("enter the b:");
+
+b=sc.nextInt();
+
+div=a/b;
+
+System.out.println(+div);
+
+
+}
+
+
+
+}
