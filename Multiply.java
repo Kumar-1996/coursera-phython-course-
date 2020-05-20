5b3e5c38ef9039e2abd74fab24c5e8a4fc8166a8
Rishabh Kumar <65464723+Kumar-1996@users.noreply.github.com>
2020-05-21 00:54:04 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Multiply{
+public static void main(String[] args) {
+	
+Scanner  sc= new Scanner(System.in);
+int a,b,m;
+System.out.println("enter the a:");
+a=sc.nextInt();
+System.out.println("enter the b:");
+b=sc.nextInt();
+m = a*b;
+
+System.out.println(" value is-"+ m);
+ 
+
+
+}
+
+
+
+}
